f1abd211d24ee992526ebd157f3bca75971ec03a
Hassen Gaaya <architecture.best.practices@gmail.com>
2022-04-23 19:56:32 +0200

    Add turning capacity

--- src/main/java/hq/Rover.java
+++ src/main/java/hq/Rover.java
@@ -2,25 +2,59 @@ package hq;
 
 import hq.enums.Direction;
 import hq.model.Coordinates;
+import hq.model.Position;
 
 public class Rover {
-    private final Coordinates coordinates;
-    private final Direction direction;
+    public static final char MOVE_BACKWARD = 'b';
+    public static final char MOVE_FORWARD = 'f';
+    public static final char TURN_RIGHT = 'r';
+    public static final char TURN_LEFT = 'l';
+    private final Position position;
 
-    public Rover(Coordinates coordinates, Direction direction) {
-
-        this.coordinates = coordinates;
-        this.direction = direction;
+    public Rover(Position position) {
+        this.position = position;
     }
 
-    public Coordinates move(char command) {
-        if ('b' == command) {
-            return moveBackward(coordinates, direction);
+    public Position move(char ...commands) {
+        for (char command : commands) {
+            if (TURN_RIGHT == command) {
+                turnRight();
+            } else if (TURN_LEFT == command) {
+                turnLeft();
+            }else if (MOVE_BACKWARD == command) {
+                moveBackward();
+            } else if (MOVE_FORWARD == command) {
+                moveForward();
+            }
         }
-        return moveForward(coordinates, direction);
+        return position;
     }
 
-    private Coordinates moveForward(Coordinates coordinates, Direction direction) {
+    private void turnRight() {
+        if (Direction.E == position.getDirection())
+            position.setDirection(Direction.S);
+        else if (Direction.S == position.getDirection())
+            position.setDirection(Direction.W);
+        else if (Direction.W == position.getDirection())
+            position.setDirection(Direction.N);
+        else
+            position.setDirection(Direction.E);
+    }
+
+    private void turnLeft() {
+        if (Direction.N == position.getDirection())
+            position.setDirection(Direction.W);
+        else if (Direction.W == position.getDirection())
+            position.setDirection(Direction.S);
+        else if (Direction.S == position.getDirection())
+            position.setDirection(Direction.E);
+        else
+            position.setDirection(Direction.N);
+    }
+
+    private void moveForward() {
+        Coordinates coordinates = position.getCoordinates();
+        Direction direction = position.getDirection();
         int x = coordinates.getX();
         int y = coordinates.getY();
         if (Direction.N.equals(direction)) {
@@ -40,10 +74,11 @@ public class Rover {
         }
         coordinates.setY(y);
         coordinates.setX(x);
-        return coordinates;
     }
 
-    private Coordinates moveBackward(Coordinates coordinates, Direction direction) {
+    private void moveBackward() {
+        Coordinates coordinates = position.getCoordinates();
+        Direction direction = position.getDirection();
         int x = coordinates.getX();
         int y = coordinates.getY();
         if (Direction.N.equals(direction))
@@ -63,6 +98,5 @@ public class Rover {
 
         coordinates.setY(y);
         coordinates.setX(x);
-        return coordinates;
     }
 }
---
+++ src/main/java/hq/model/Position.java
@@ -0,0 +1,26 @@
+package hq.model;
+
+import hq.enums.Direction;
+
+public class Position {
+    private Coordinates coordinates;
+    private Direction direction;
+
+    public Position(Coordinates coordinates, Direction direction) {
+        this.coordinates = coordinates;
+        this.direction = direction;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
